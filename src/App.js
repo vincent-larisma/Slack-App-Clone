f1cab62ca14fd829d9d72efd3320538c1c51c501
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-17 20:21:28 +0800

    sendfunction draft

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-<<<<<<< Updated upstream
+
 
 function App() {
   return <></>
-=======
+
 import LoginFunction from './Components/LoginFunction'
 import RegisterFunction from './Components/RegisterFunction'
 import SendMessageFunction from './Components/SendMessageFunction'
@@ -16,7 +16,7 @@ function App() {
       {<SendMessageFunction />}
     </>
   )
->>>>>>> Stashed changes
+
 }
 
 export default App
--- src/Components/RegisterFunction.jsx
+++ src/Components/RegisterFunction.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function RegisterFunction() {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  })
+
+  const { email, password, passwordConfirm } = userData
+
+  const userDataAPI = {
+    email: email,
+    password: password,
+    password_confirmation: passwordConfirm,
+  }
+
+  const handleChangeEmail = (event) => {
+    const { name, value } = event.target
+    setUserData({ ...userData, [name]: value })
+  }
+  const handleChangePassword = (event) => {
+    const { name, value } = event.target
+    setUserData({ ...userData, [name]: value })
+  }
+  const handleChangePasswordConfirm = (event) => {
+    const { name, value } = event.target
+    setUserData({ ...userData, [name]: value })
+  }
+
+  const APIurl = 'http://206.189.91.54/api/v1'
+
+  const handleClickSubmit = (event) => {
+    event.preventDefault()
+    fetch(`${APIurl}/auth/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userDataAPI),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+
+    setUserData({ ...userData, email: '', password: '', passwordConfirm: '' })
+  }
+
   return (
-    <div>RegisterFunction</div>
+    <>
+      <div className='form-container'>
+        <div className='form-name'>Create User</div>
+        <form>
+          <div>
+            <label htmlFor='email'>Email:</label>
+            <br />
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email address'
+              onChange={handleChangeEmail}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password:</label>
+            <br />
+            <input
+              type='text'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter your password'
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor='passwordConfirm'>Confirm Password:</label>
+            <br />
+            <input
+              type='text'
+              id='passwordConfirm'
+              name='passwordConfirm'
+              value={passwordConfirm}
+              placeholder='Enter your password confirmation'
+              onChange={handleChangePasswordConfirm}
+            />
+          </div>
+          <button onClick={handleClickSubmit}>Submit</button>
+        </form>
+      </div>
+    </>
   )
 }
