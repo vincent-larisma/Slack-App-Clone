de7f5282e458705ab1cf7b6749a5564d85358d90
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-17 20:15:34 +0800

    sendfunction draft

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+<<<<<<< Updated upstream
 
 function App() {
   return <></>
+=======
+import LoginFunction from './Components/LoginFunction'
+import RegisterFunction from './Components/RegisterFunction'
+import SendMessageFunction from './Components/SendMessageFunction'
+
+function App() {
+  return (
+    <>
+      {/* <LoginFunction /> */}
+      {/* <RegisterFunction /> */}
+      {<SendMessageFunction />}
+    </>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App
