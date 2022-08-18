1a544d57e11c89ec997ca8039d48163db746d1ff
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-18 11:52:09 +0800

    use useContext, add file loginInfo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { LoginProvider } from './Components/LoginContext'
 import LoginFunction from './Components/LoginFunction'
 import RegisterFunction from './Components/RegisterFunction'
 import SendMessageFunction from './Components/SendMessageFunction'
@@ -7,12 +8,14 @@ import SendMessageFunction from './Components/SendMessageFunction'
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/CreateUser' element={<RegisterFunction />}></Route>
-        <Route path='/Login' element={<LoginFunction />}></Route>
-        <Route path='/' element={<LoginFunction />}></Route>
-        <Route path='/Test' element={<SendMessageFunction />}></Route>
-      </Routes>
+      <LoginProvider>
+        <Routes>
+          <Route path='/CreateUser' element={<RegisterFunction />}></Route>
+          <Route path='/Login' element={<LoginFunction />}></Route>
+          <Route path='/' element={<LoginFunction />}></Route>
+          <Route path='/Test' element={<SendMessageFunction />}></Route>
+        </Routes>
+      </LoginProvider>
     </>
   )
 }
---
+++ src/Components/LoginContext.jsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from 'react'
+
+export const LoginContext = createContext()
+
+export const LoginProvider = ({ children }) => {
+  const [loginInfo, setLoginInfo] = useState()
+  return <LoginContext.Provider value={{ loginInfo, setLoginInfo }}>{children}</LoginContext.Provider>
+}
