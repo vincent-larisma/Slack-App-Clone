624ba788498ac54cafb0e14340acf946ced84970
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-20 16:57:33 +0800

    Update LoginContext.jsx

--- src/Components/LoginContext.jsx
+++ src/Components/LoginContext.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from 'react'
 export const LoginContext = createContext()
 export const LoginContextHeader = createContext()
 
+
 export const LoginProvider = ({ children }) => {
   const [loginInfo, setLoginInfo] = useState({})
   const [loginInfoHeader, setLoginInfoHeader] = useState({})
