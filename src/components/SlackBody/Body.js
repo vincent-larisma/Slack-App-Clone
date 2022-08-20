d9d9878da2ce52a2ff45f168d4e74c2fe566f3ae
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-20 10:29:31 +0800

    user-name

--- src/components/SlackBody/Body.js
+++ src/components/SlackBody/Body.js
@@ -7,13 +7,15 @@ function Body() {
 
   const [activeName, setactiveName] = useState("Shawn Go");
 
+  const [availUser, setavailUser] = useState("Vince Larisma")
+
   const [channgelToggle, setchannelToggle] = useState(false);
   const [directMessageToggle, setdirectMessageTogggle] = useState(false);
 
   const toggleChannel = () => {
     channgelToggle ? setchannelToggle(false) : setchannelToggle(true);
     console.log(channgelToggle)
-  }
+  } 
 
   const toggledirectMessage = () => {
     directMessageToggle ? setdirectMessageTogggle(false) : setdirectMessageTogggle(true);
@@ -25,43 +27,40 @@ function Body() {
       <div className='body-container'> 
         <section className='threads'>
             <div className='threads-buttons'>
-                
                 <button onClick={toggledirectMessage}>
-                    <i class="fa-solid fa-message"></i> Direct messages
+                  <i class="fa-solid fa-message"></i> Direct messages
                 </button>
                   <div className='direct-message-channel'>
                     <ul className={directMessageToggle ? "channel-names-clicked " : "channel-names-not-clicked"}>
-                        <li>Justine Jun Banogon</li>
-                        <li>Vince Larisma</li>
+                      <li>{availUser}</li>
                     </ul>
                   </div>
                 <button onClick={toggleChannel}>
-                    <i class="fa-solid fa-people-group"></i> Channels
+                  <i class="fa-solid fa-people-group"></i> Channels
                 </button>
                   <div className='names-channel'>
                     <ul className={channgelToggle ? "channel-names-clicked " : "channel-names-not-clicked"}>
-                        <li>batch21</li>
-                        <li>group 2 - Slack App</li>
+                      <li>batch21</li>
+                      <li>group 2 - Slack App</li>
                     </ul>
                   </div>
                 <button>
-                    <i class="fa-solid fa-bell"></i> Activity    
+                  <i class="fa-solid fa-bell"></i> All users    
                 </button>
             </div>
         </section>
         <section className='message-window'>
             <section className='top-body-section'>
               <div className='name-converse'>
-                <button>{activeName} &nbsp; <i class="fa-solid fa-caret-down"></i></button>
+                <button>{availUser} &nbsp; <i class="fa-solid fa-caret-down"></i></button>
               </div>
               <div className='new-chat-btn'>
                 <i class="fa-solid fa-circle-plus"></i>
               </div>
             </section>
-
             <section className='conversation'>
               <div className='sender-container'>
-                <div className='message-sender-name'><i class="fa-solid fa-circle"></i>Evan Maylas</div>
+                <div className='message-sender-name'>Evan Maylas <i class="fa-solid fa-circle"></i></div>
                   <p className='sender-chat'>Ang panget mo</p>
               </div>
               <div className='receiver-container'>
@@ -69,7 +68,6 @@ function Body() {
                   <p className="receiver-chat">Mas panget ka</p>
               </div>
             </section>
-
             <section className='new-message'>
               <textarea id="sendmessage" name="sendmessage" placeholder='Write your message...'>
               </textarea>
