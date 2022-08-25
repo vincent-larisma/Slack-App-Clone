c31a4a78aa635f07044370a826c08cf98b2e0933
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-25 14:00:15 +0800

    login/register

--- src/Components/LoginFunction.jsx
+++ src/Components/LoginFunction.jsx
@@ -75,8 +75,9 @@ export default function LoginFunction() {
     <div className='login-container'>
       <form className='form-container'>
         <section className='inner-form'>
-          <div className='form-name'>Login to Slack App</div>
+          <div className='form-name'>Slack App <i class="fa-solid fa-square-envelope"></i></div>
           <div className='login-email'>
+            <p className='top-credentials'>Username <i class="fa-solid fa-user"></i></p>
             <input
               className='login-email-input'
               type='text'
@@ -87,6 +88,8 @@ export default function LoginFunction() {
               onChange={handleChangeEmail}
             />
           </div>
+
+          <p className='top-credentials'>Password <i class="fa-solid fa-lock"></i></p>
           <div className='login-password'>
             <input
               className='login-password-input'
